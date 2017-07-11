f5e119b516b8a4f2b442674f36b053e17e1c4ac3
Blake Bertuccelli <blakebertuccelli@gmail.com>
2017-07-11 13:02:34 -0500

    Update Banner Text

--- template_parts/loop_content-nolavie_notice.php
+++ template_parts/loop_content-nolavie_notice.php
@@ -5,7 +5,7 @@ if(has_tag('nolavie-archive')):
 ?>
 
 <div class="loop_content-nolavie_notice">
-  Does content look wrong? <a class="nolavie_notice-report_link" href="<?php echo get_permalink(8309).'?page+url='.get_permalink(); ?>">Click here</a> to report any errors.
+  Does this content look wrong? <a class="nolavie_notice-report_link" href="<?php echo get_permalink(8309).'?page+url='.get_permalink(); ?>">Click here</a> to report any errors.
 </div>
 
 <?php

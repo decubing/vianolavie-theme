0a661d3d1b7d77c9dcb3d09ed0bd2681c19e5728
Ezra Bertuccelli <comradehiggins@gmail.com>
2019-02-19 12:46:12 -0800

    Show badges in block editor

--- includes/custom_taxonomies.php
+++ includes/custom_taxonomies.php
@@ -42,6 +42,7 @@ function vnv_add_location_taxonomy() {
 		'hierarchical' => true,
 		'show_ui' => true,
 		'show_admin_column' => true,
+		'show_in_rest' => true,
 		'query_var' => true,
 		'rewrite' => true,
 	);
